3e9a5f440e719f506023abf4147633607c37977f
Kirill Shmidt <83780823+KirillSchmidt@users.noreply.github.com>
2025-06-17 02:42:18 +0300

    funny custom dice validation
    - made the `generate_dice_result()` be a private function, and made a `pub fn custom_dice()` instead. (I might change its' signature soon though, to accept both the min and the max, just with sanitisation)
    - added some easter eggs too, because who's gonna stop me, right?
    - moved the ascii_art into a /gui folder

--- src/dice_generation.rs
+++ src/dice_generation.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 
-pub fn generate_dice_result(min: i8, max: i8) -> i8 {
+fn generate_dice_result(min: i8, max: i8) -> i8 {
     // TODO: do checks for max > min || max >= min
     let random_number = rand::rng().random_range(min..=max);
     return random_number;
@@ -16,4 +16,23 @@ pub fn flip_coin() -> String {
 
 pub fn standard_dice() -> u8 {
     return generate_dice_result(1, 6) as u8;
+}
+
+pub fn custom_dice(side: i8) -> Result<i8, String> {
+    if side <= 0 {
+        return Err("The value should be larger than 0".into());
+    }
+    let result = generate_dice_result(1, side);
+    if side > 2 {
+        return Ok(result);
+    }
+    // Let the fun begin!
+    let mut funny_error_msg = String::new();
+    if side == 1 {
+        funny_error_msg.push_str("It doesn't make any sense! It will always be equal to 1");
+    } else if side == 2 {
+        funny_error_msg.push_str("Just use the 'Flip a coin' option...");
+    }
+    funny_error_msg += &format!("\n(it's {} btw)", result);
+    return Err(funny_error_msg);
 }
