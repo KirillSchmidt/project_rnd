922fd9612fa49ea12fc71ce4f5b458c088c5a460
Kirill Shmidt <83780823+KirillSchmidt@users.noreply.github.com>
2025-06-17 23:23:00 +0300

    made clippy less annoyed at me

--- src/gui/ascii_graphics.rs
+++ src/gui/ascii_graphics.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use artem::config::Config;
 use artem::ConfigBuilder;
 use std::num::NonZeroU32;
@@ -12,7 +13,7 @@ fn create_config() -> Config {
 pub fn display_standard_dice(side: u8) {
     assert!((1..=6).contains(&side));
     let filepath = format!("resources/img/face-{side}.jpeg");
-    std::fs::exists(&filepath).unwrap();
+    assert!(std::fs::exists(&filepath).unwrap());
     let img = image::open(filepath).expect("Failed to open an image");
     let ascii_art = artem::convert(img, &create_config());
     println!("{}", ascii_art);
